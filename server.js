4dfc90a36d068ac2db0fd6c75142f21f59fe7e1c
destinynkemjika <destinynkemjika2006@gmail.com>
2025-05-05 16:49:23 +0100

    Update server.js for Render deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://cryptosocial.vercel.app", // Replace with your Netlify URL
+    origin: "https://solreach.net", // Replace with your Netlify URL
     methods: ["GET", "POST"],
   },
 });
